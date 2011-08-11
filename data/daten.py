cc7784fc62befbd4640c04235369b0d3a7a69fc0
Olaf Merkert <olaf@m-merkert.de>
2011-08-11 14:16:22 +0200

    Datenstrukturen für eine Arbeitsverteilungssoftware in Python

---
+++ data/daten.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import with_statement
+
+__author__ = "Olaf Merkert"
+
+
+import cPickle as pickle
+import models
+
+# Listen für die Speicherung der Vorlesungen und Assistenten sowie
+# ihrer Wünsche
+
+# TODO Eigene Datenstruktur für diese Listen?
+class Collector(object):
+    """
+    Halte eine Liste/Menge von Objekten vor.
+    """
+    
+    def __init__(self):
+        self._list = []
+
+    def add(self, item):
+        self._list.append(item)
+
+    def rem(self, item):
+        self._list.rem(item)
+
+    def load(self, liste):
+        self._list = liste
+        
+    def clear(self):
+        self._list = []
+
+    def __iter__(self):
+        return self._list
+
+    def save(self):
+        return self._list
+
+    def __repr__(self):
+        return "Collector: " + str(self._list)
+
+taetigkeiten = Collector()
+assistenten  = Collector()
+
+# Ein paar hartkodierte Einstellungen
+bereiche = [
+    "Grundstudium",
+    "Aufbaustudium",
+    "Master",
+    ]
+
+# laden und speichern der daten
+
+default_store = "/tmp/avpy.bin"
+
+
+def load(store = default_store):
+    def get_if_present(key, dct):
+        if key in dct:
+            return dct[key]
+        else:
+            return []
+    with open(store, 'rb') as input:
+        data = pickle.load(input)
+        taetigkeiten.load(get_if_present("taet", data))
+        assistenten.load(get_if_present("ass", data))
+    return data
+
+def save(store = default_store):
+    with open(store, 'wb') as output:
+        pickle.dump({"taet" : taetigkeiten.save(),
+                     "ass"  : assistenten.save(),
+             }, output)
---
+++ data/models.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Olaf Merkert"
+
+# Modelle für die Arbeitsverteilung-Anwendung
+
+class Bound(object):
+    def __init__(self, lower, upper=None):
+        self._lower = lower
+        if upper:
+            self._upper = upper
+        else:
+            self._upper = lower
+
+class Taetigkeit(object):
+    """
+    Jede Vorlesung und jedes Seminar benötigt eine gewisse Anzahl von
+    Assistenten.
+    """
+    
+    def __init__(self, titel, bedarf, dozent = "", bereich = "", termin = "", bemerkung = ""):
+        self._titel = titel
+        self._bedarf = Bound(bedarf)
+        self._dozent = dozent
+        self._bereich = bereich
+        self._termin = termin
+        self._bemerkung = bemerkung
+
+class Wunsch(object):
+    """
+    Mit verschiedenem Nachdruck kann man die gewuenschten
+    Veranstaltungen sortieren.
+    """
+    
+    def __init__(self, assistent, taetigkeit, staerke):
+        self._assistent = assistent
+        self._taetigkeit = taetigkeit
+        self._staerke = staerke
+
+    def zuweisen(self, tabelle):
+        z = Zuweisung(self._assistent,
+                      self._taetigkeit,
+                      self.score())
+        tabelle.add(z)
+        return z
+
+    def score(self):
+        return (4 - self._staerke)**2
+
+
+
+class Assistent(object):
+    """
+    Jeder Assistent hat 1 oder mehr Übungsstunden zu halten.  Vor der
+    Verteilung dürfen mehrere Wünsche geäußert werden.
+    """
+    
+    def __init__(self, name, bedarf = 1):
+        self._name = name
+        self._bedarf = Bound(bedarf)
+        self._wuensche = set()
+
+    def wuenschen(self, taetigkeit, staerke):
+        self._wuensche.add(
+            Wunsch(self, taetigkeit, staerke))
+
+class Zuweisung(object):
+    """
+    Gebe einem Assistenten eine bestimmte Vorlesung.
+    """
+    
+    def __init__(self, assistent, taetigkeit, score = 0, fest = False):
+        self._assistent = assistent
+        self._taetigkeit = taetigkeit
+        self._score = score
+        self._fest = fest
+        
+
