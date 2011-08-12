fb68eaad7e936413e0e515327bb322b399cdc4bc
Olaf Merkert <olaf@m-merkert.de>
2011-08-12 10:52:13 +0200

    Automatisiere die Erstellung von Eingabeformularen

--- ui/data_entry.py
+++ ui/data_entry.py
@@ -3,10 +3,10 @@
 
 __author__ = "Olaf Merkert"
 
-import sys
-from PyQt4 import QtGui
+# import sys
+from PyQt4 import QtGui, QtCore
 from PyQt4.QtCore import QObject, pyqtSignal, pyqtSlot
-
+ 
 # Ein paar spezielle Funktionen und Dialoge, die speziell für die
 # Dateneingabe angepasst sind.
 
@@ -21,7 +21,8 @@ class SaveNextDialog (QtGui.QWidget):
     close_signal = pyqtSignal()
 
     def __init__(self):
-        QtGui.QDialog.__init__(self)
+        QtGui.QWidget.__init__(self)
+        self.setLayout(QtGui.QVBoxLayout())
         self.setupUi()
         self.setup_buttons()
 
@@ -33,11 +34,12 @@ class SaveNextDialog (QtGui.QWidget):
         _save   = QtGui.QPushButton(text="Speichern")
         _cancel = QtGui.QPushButton(text="Abbrechen")
         # Gruppiere die Knöpfe in einer HBox
-        self._button_box = QtGui.QHBoxLayout()
-        # self._button_box.layout.addStretch()
-        self._button_box.layout.addWidget(_next)
-        self._button_box.layout.addWidget(_save)
-        self._button_box.layout.addWidget(_cancel)
+        self._button_box = QtGui.QWidget()
+        self._button_box.setLayout(QtGui.QHBoxLayout())
+        self._button_box.layout().addStretch()
+        self._button_box.layout().addWidget(_next)
+        self._button_box.layout().addWidget(_save)
+        self._button_box.layout().addWidget(_cancel)
         # Lasse Signale erzeugen
         @pyqtSlot()
         def next_clicked():
@@ -54,13 +56,70 @@ class SaveNextDialog (QtGui.QWidget):
         _save.clicked.connect(save_clicked)
         _cancel.clicked.connect(cancel_clicked)
         # Stecke die Box ans Ende
-        self.layout.addWidget(self._button_box)
+        self.layout().addStretch()
+        self.layout().addWidget(self._button_box)
 
-def run_widget(w):
-    app = QtGui.QApplication([])
-    i = w()
-    i.show()
-    sys.exit(app.exec_())
+class EntryFormDialog (SaveNextDialog):
+    """
+    Erzeuge ein Eingabeformular durch Angeben eines Titel und
+    """
 
-if __name__ == '__main__':
-    run_widget(SaveNextDialog)
+    new_data_signal = pyqtSignal(dict)
+
+    def __init__(self, title, form_elements):
+        SaveNextDialog.__init__(self)
+        self.setWindowTitle(title)
+        self._form.setTitle(title)
+        self.setup_inputs(form_elements)
+        self.save_signal.connect(self.on_save)
+        self.close_signal.connect(self.on_close)
+        # Damit der Dialog nicht geschlossen wird, bevor alle Daten
+        # ausgelesen sind, brauchen wir ein kleines Lock.
+        self.close_lock = QtCore.QMutex()
+
+    def setupUi(self):
+        # Die Eingabefelder kommen in eine GroupBox.  Der Titel wird
+        # später gesetzt.
+        self._form = QtGui.QGroupBox()
+        self._form.setLayout(QtGui.QFormLayout())
+        self.layout().addWidget(self._form)
+
+    def setup_inputs(self, form_elements):
+        self._inputs = {}
+        for label, widget in form_elements:
+            l = QtGui.QLabel(label)
+            i = widget
+            # Die Inputs halten wir in einem dict vor
+            self._inputs[label] = i
+            self._form.layout().addRow(l, i)
+
+    def load(self, data):
+        """
+        Fülle die Formularfelder mit Werten aus dem gegebenen dict,
+        oder setze zurück, falls kein Wert gegbenen ist.
+        """
+        for l,i in self._inputs.iteritems():
+            if l in data:
+                i.setData(data[l])
+            else:
+                i.setData()
+                
+    @pyqtSlot()
+    def on_save(self):
+        self.close_lock.lock()
+        # Hole alle Daten aus den Eingabefeldern
+        data = {}
+        for l,i in self._inputs.iteritems():
+            data[l] = i.data()
+        # Setze alle Eingabefelder zurück
+        self.load({})
+        self.new_data_signal.emit(data)
+        self.close_lock.unlock()
+
+    @pyqtSlot()
+    def on_close(self):
+        self.close_lock.lock()
+        # Schließe das Fenster
+        self.close()
+        self.close_lock.unlock()
+        
---
+++ ui/inputs.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Olaf Merkert"
+
+from PyQt4 import QtGui
+
+# TODO Bewege dies zu einer gemeinsamen Position
+class Unimplemented (object):
+    pass
+
+class InputBase(object):
+    """
+    Interface für Eingabeelemente in Formularen
+    """
+
+    def default_value(self):
+        raise Unimplemented
+    
+    def data(self):
+        raise Unimplemented
+
+    def setData(self, value = None):
+        if value == None:
+            self.setData(self.default_value())
+        else:
+            self._setData(value)
+
+    def _setData(self, value):
+        raise Unimplemented
+
+class StringInput(QtGui.QLineEdit, InputBase):
+    """
+    Eingabe von Strings mit einem QLineEdit.
+    """
+
+    def __init__(self, *parms):
+        QtGui.QLineEdit.__init__(self, *parms)
+
+    def default_value(self):
+        return ""
+
+    def data(self):
+        return str(self.text())
+
+    def _setData(self, value):
+        self.setText(value)
+
+class IntegerInput(QtGui.QSpinBox, InputBase):
+    """
+    Eingabe von ganzen Zahlen mit einer QSpinBox.  Als Parameter kann
+    ein Standardwert gegeben werden.
+    """
+
+    def __init__(self, default = 1, *parms):
+        QtGui.QSpinBox.__init__(self, *parms)
+        self._default = default
+        self._setData(default)
+
+    def default_value(self):
+        return self._default
+
+    def data(self):
+        return self.value()
+
+    def _setData(self, value):
+        self.setValue(value)
+
+class SelectInput(QtGui.QComboBox, InputBase):
+    """
+    Auswahl eines Eintrags aus einer Liste.  Als Parameter wird eine
+    Liste von Tupeln erwartet, deren erster Eintrag ein String, der
+    zweite ein beliebiges Objekt sein kann.  Der ausgewählte Index
+    wird transparent zu den jeweiligen Objekten umgewandelt.
+    """
+    
+    def __init__(self, underlying_list = [], *parms):
+        QtGui.QComboBox.__init__(self, *parms)
+        self._underlying = underlying_list
+        for l,i in underlying_list:
+            self.addItem(l, i)
+
+    def data(self):
+        return self._underlying[self.currentIndex()][1]
+
+    def setData(self, value = None):
+        if value == None:
+            self.setCurrentIndex(0)
+        else:
+            i = self.findData(value)
+            self.setCurrentIndex(i)
+
+def generate_labels(lst):
+    """
+    Transformiere eine Liste für die Benutzung mit SelectInput.  Jedem
+    Element wird ein Tupel String-Repräsentation, Element zugeordnet.
+    """
+    return [[str(l), l] for l in lst]
